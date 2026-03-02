740aab878df8b5ecddc376203362248b62ac31d3
Soonyee1346 <92239495+Soonyee1346@users.noreply.github.com>
2026-03-03 08:16:37 +1100

    Frontend finished

--- backend.js
+++ backend.js
@@ -22,11 +22,20 @@ function getDashboardData() {
   });
 }
 
-function processRoomAction(actionType, indicesArray) {
+function processRoomAction(actionType, indicesArray, location, region, roomName) {
   const logSheet = SpreadsheetApp.openById(LOGS_SPREADSHEET_ID).getSheetByName('CurrentOpenIssues');
-  
+  const targetSpreadsheetID = REGION_CONFIG[region].spreadsheetID;
+
+  const sheetName = location + " Meet Device Status"
+
+  const regionSheet = SpreadsheetApp.openById(targetSpreadsheetID).getSheetByName(sheetName);
+  if (!regionSheet) throw new Error(`Sheet not found: ${sheetName}`);
+  const regionData = regionSheet.getDataRange().getValues();
+
+  let rowIndex = regionData.findIndex(row => row[2] === roomName) + 1;
+
   indicesArray.forEach(index => {
-    const sheetRow = index + 2; 
+    const sheetRow = index + 2;
 
     if (actionType === "Resolve") {
       logSheet.getRange(sheetRow, 8).setValue("Resolved");
@@ -35,6 +44,26 @@ function processRoomAction(actionType, indicesArray) {
     } else if (actionType === "Unignore") {
       logSheet.getRange(sheetRow, 7).clearContent();
     }
+  });
+
+  if (actionType === "Resolve") {
+    regionSheet.getRange(rowIndex, 12).setValue(true);
+  } else if (actionType === "Ignore") {
+    regionSheet.getRange(rowIndex, 11).setValue(true);
+  } else if (actionType === "Unignore") {
+    regionSheet.getRange(rowIndex, 11).setValue(false);
+  };
+
+  SpreadsheetApp.flush();
+}
+
+function updateRoomNotes(noteText, indicesArray) {
+  const logSheet = SpreadsheetApp.openById(LOGS_SPREADSHEET_ID).getSheetByName('CurrentOpenIssues');
+  
+  indicesArray.forEach(index => {
+    const sheetRow = index + 2;
+    
+    logSheet.getRange(sheetRow, 9).setValue(noteText); 
   });
 
   SpreadsheetApp.flush();
